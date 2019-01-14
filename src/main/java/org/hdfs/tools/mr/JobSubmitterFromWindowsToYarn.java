8d82e254ca73feb1f676473e7eaa4cda19eb159f
midnightYTK <ytk_jeson@163.com>
2019-01-14 14:48:49 +0800

    修正错误，JobSubmitterFromWindowsToYarn存在问题，发现conf.set()配置的参数未生效，但是通过conf.addResource()配置的参数生效了，具体原因仍在核查中。 另，wc.jar在src/main/java/路径下。

--- src/main/java/org/hdfs/tools/mr/JobSubmitterFromWindowsToYarn.java
+++ src/main/java/org/hdfs/tools/mr/JobSubmitterFromWindowsToYarn.java
@@ -14,6 +14,12 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
+/**
+ * 经测试，通过conf.set()的方式配置参数并没有生效，但是通过conf.addResource()的方式配置的参数生效了，目前依然在发现问题中。
+ * 
+ * @author TheDK
+ *
+ */
 public class JobSubmitterFromWindowsToYarn {
 	
 	public static void main (String []args) throws IOException, InterruptedException, URISyntaxException, ClassNotFoundException {
@@ -21,34 +27,55 @@ public class JobSubmitterFromWindowsToYarn {
 		Properties properties = new Properties();
 		properties.load(JobSubmitter.class.getClassLoader().getResourceAsStream("conf.properties"));
 		
+		// 获取参数
+		String defaultFS = properties.getProperty("HDFS_ADDRESS");
+		String mrFwName = properties.getProperty("mapreduce.framework.name");
+		String yarnResourseManager = properties.getProperty("yarn.resoursemanager");
+		String crossPlatformOrNot = properties.getProperty("mapreduce.app_submission.cross_platform");
+		String jarPath = properties.getProperty("JobJarPath");
+		String URIPath = properties.getProperty("HDFS_ADDRESS");
+		String USERNAME = properties.getProperty("USERNAME");
+		String HADOOP_USER_NAME = properties.getProperty("HADOOP_USER_NAME");
+		
+		Path mrOutputPath = new Path(properties.getProperty("MROUTPUT_PATH"));
+		Path mrInputPath = new Path(properties.getProperty("MRINPUT_PATH"));
+		
+		
 		// 在代码中配置JVM参数，用于给job对象来获取访问HDFS的用户身份
-		System.setProperty("HADOOP_USER_NAME", properties.getProperty("HADOOP_USER_NAME"));
+		System.setProperty("HADOOP_USER_NAME", HADOOP_USER_NAME);
 		
 		Configuration conf = new Configuration();
-		conf.set("fs.defaultFS", properties.getProperty("HDFS_ADDRESS"));
+		conf.set("fs.defaultFS", defaultFS);
 		// 默认为local
-		conf.set("mapreduce.framework.name", properties.getProperty("mapreduce.framework.name"));
-		conf.set("yarn.resoursemanager", properties.getProperty("yarn.resoursemanager"));
+		conf.set("mapreduce.framework.name", mrFwName);
+		conf.set("yarn.resoursemanager", yarnResourseManager);
 		// 配置跨平台提交参数
-		conf.set("mapreduce.app_submission.cross_platform", properties.getProperty("mapreduce.app_submission.cross_platform"));
+		conf.set("mapreduce.app_submission.cross_platform", crossPlatformOrNot);
+		
+		
+		conf.addResource("core-site.xml");
+        conf.addResource("hdfs-site.xml");
+        conf.addResource("mapred-site.xml");
+        conf.addResource("yarn-site.xml");
 		
 		
 		Job job = Job.getInstance(conf);
 		
 		// 1. 封装参数，jar所在的位置
-		job.setJar(properties.getProperty("JobJarPath"));
+		job.setJar(jarPath);
 		
 		// 2. 配置mapper reducer
 		job.setMapperClass(WordcountMapper.class);
 		job.setReducerClass(WordcountReducer.class);
 		// 3. 配置输出的k v参数
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(IntWritable.class);
+		
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(IntWritable.class);
 		
-		Path mrOutputPath = new Path(properties.getProperty("MROUTPUT_PATH"));
-		Path mrInputPath = new Path(properties.getProperty("MRINPUT_PATH"));
 		FileSystem fs = FileSystem.get(
-				new URI(properties.getProperty("HDFS_ADDRESS")), conf, properties.getProperty("USERNAME"));
+				new URI(URIPath), conf, USERNAME);
 		if( fs.exists(mrOutputPath) ) {
 			fs.delete(mrOutputPath, true);
 		}

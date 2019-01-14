39dd97cb1564bcca4da1765c9515d50f5a40afa0
midnightYTK <ytk_jeson@163.com>
2019-01-14 15:24:07 +0800

    写了个一个递归删除目录下子目录和文件的方法，解决JobSubmitterAtWindowsLocal第二次执行报错问题。

--- src/main/java/org/hdfs/tools/mr/JobSubmitterAtWindowsLocal.java
+++ src/main/java/org/hdfs/tools/mr/JobSubmitterAtWindowsLocal.java
@@ -11,38 +11,52 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
 public class JobSubmitterAtWindowsLocal {
-	
+
 	public static void main(String[] args) throws Exception {
-		
+
 		Configuration conf = new Configuration();
-		
+
 		// 如果在windows本地执行，则无需配置（仍然需要配置Windows的HADOOP_HOME），默认情况下的fs.FileSystem是local
 		Job job = Job.getInstance(conf);
-		
+
 		job.setJarByClass(JobSubmitterLinuxToYarn.class);
-		
+
 		job.setMapperClass(WordcountMapper.class);
 		job.setReducerClass(WordcountReducer.class);
-		
+
 		job.setMapOutputKeyClass(Text.class);
 		job.setMapOutputValueClass(IntWritable.class);
-		
+
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(IntWritable.class);
-		
+
 		File file = new File("E:/Workbench/mrdata/wordcount/output");
-		if( file.exists() ) {
-			file.delete();
+		if (file.exists()) {
+			delAllFile(file);
 		}
-		
-		
+
 		FileInputFormat.setInputPaths(job, new Path("E:/Workbench/mrdata/wordcount/input"));
 		FileOutputFormat.setOutputPath(job, new Path("E:/Workbench/mrdata/wordcount/output"));
-		
+
 		job.setNumReduceTasks(1);
-		
+
 		boolean res = job.waitForCompletion(true);
-		System.exit(res?0:1);
-		
+		System.exit(res ? 0 : 1);
+
 	}
+
+	public static void delAllFile(File directory) {
+		if (!directory.isDirectory()) {
+			directory.delete();
+		} else {
+			File[] files = directory.listFiles();
+			if (files.length != 0)
+				for (File file : files) {
+					delAllFile(file);
+				}
+			directory.delete();
+			return;
+		}
+	}
+
 }
